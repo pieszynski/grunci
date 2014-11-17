56ba6c13618fa73ed54306ca1e536f831b64e491
Przemysław Pieszyński <przemyslaw.pieszynski@mbank.pl>
2014-11-17 17:03:37 +0100

    Stream build process - DEV build only (for now)

--- www/static/js/index.ts
+++ www/static/js/index.ts
@@ -7,6 +7,91 @@ interface IIndexController {
     getProjectsList() : string[];
 }
 
+var StreamAjax;
+(function(sjax) {
+
+    var StreamClass = function (method, url) {
+
+        var nop = function () {}
+
+        this.url = url;
+        this.method = method || 'post';
+
+        this.onProgress = nop;
+        this.onError = nop;
+        this.onSuccess = nop;
+    };
+    StreamClass.prototype = function() {
+
+        var progress = function(callback) {
+
+                this.onProgress = callback;
+
+                return this;
+
+            },
+            error = function(callback) {
+
+                this.onError = callback;
+
+                return this;
+
+            },
+            success = function(callback) {
+
+                this.onSuccess = callback;
+
+                return this;
+
+            },
+            execute = function() {
+
+                var _this = this;
+                _this.xhr = new XMLHttpRequest();
+                _this.xhr.open(_this.method, _this.url, true);
+                _this.xhr.onprogress = function () {
+
+                    _this.onProgress.apply(this, arguments);
+
+                };
+                _this.xhr.onerror = function () {
+
+                    _this.onError.apply(this, arguments);
+
+                }
+                _this.xhr.onreadystatechange = function () {
+
+                    if (4 === this.readyState)
+                        _this.onSuccess.apply(this, arguments);
+
+                };
+
+                _this.xhr.send();
+
+                return _this;
+
+            };
+
+        return {
+
+            error : error,
+            execute : execute,
+            progress : progress,
+            success : success
+
+        };
+
+    }();
+
+    sjax.post = function (url, data) {
+
+        var response = new StreamClass('post', url);
+        return response;
+
+    };
+
+})(StreamAjax || (StreamAjax = {}));
+
 (() => {
 
     'use strict';
@@ -38,6 +123,20 @@ interface IIndexController {
                 self._projectsList = data;
 
             });
+
+        StreamAjax
+            .post('/project/build/build1')
+            .success(function(data) {
+
+                console.log('success:', data);
+
+            })
+            .progress(function(data) {
+
+                console.log('progress:', data);
+
+            })
+            .execute();
 
     }]);
 
