53e18eafb28a039d4cec5a6651cb002e91fe0373
pieszynski <git@pieszynski.com>
2014-11-19 00:31:03 +0100

    Extract streaming ajax api to anoter file. TODO: send ajax parameters

--- www/static/js/index.ts
+++ www/static/js/index.ts
@@ -1,5 +1,6 @@
 
 /// <reference path="../../../tsd/angular.d.ts"/>
+/// <reference path="stream-ajax.ts"/>
 
 interface IIndexController {
     _projectsList : string[];
@@ -8,91 +9,6 @@ interface IIndexController {
     getProjectsList() : string[];
     getBuildOutput() : string;
 }
-
-var StreamAjax;
-(function(sjax) {
-
-    var StreamClass = function (method, url) {
-
-        var nop = function () {}
-
-        this.url = url;
-        this.method = method || 'post';
-
-        this.onProgress = nop;
-        this.onError = nop;
-        this.onSuccess = nop;
-    };
-    StreamClass.prototype = function() {
-
-        var progress = function(callback) {
-
-                this.onProgress = callback;
-
-                return this;
-
-            },
-            error = function(callback) {
-
-                this.onError = callback;
-
-                return this;
-
-            },
-            success = function(callback) {
-
-                this.onSuccess = callback;
-
-                return this;
-
-            },
-            execute = function() {
-
-                var _this = this;
-                _this.xhr = new XMLHttpRequest();
-                _this.xhr.open(_this.method, _this.url, true);
-                _this.xhr.onprogress = function () {
-
-                    _this.onProgress.apply(this, arguments);
-
-                };
-                _this.xhr.onerror = function () {
-
-                    _this.onError.apply(this, arguments);
-
-                }
-                _this.xhr.onreadystatechange = function () {
-
-                    if (4 === this.readyState)
-                        _this.onSuccess.apply(this, arguments);
-
-                };
-
-                _this.xhr.send();
-
-                return _this;
-
-            };
-
-        return {
-
-            error : error,
-            execute : execute,
-            progress : progress,
-            success : success
-
-        };
-
-    }();
-
-    sjax.post = function (url, data) {
-
-        var response = new StreamClass('post', url);
-        return response;
-
-    };
-
-})(StreamAjax || (StreamAjax = {}));
 
 function slideBottom() {
 
@@ -151,11 +67,10 @@ function slideBottom() {
 
         }
 
-        StreamAjax
+        Ajax.StreamAjax
             .post('/project/build/build1')
             .success(onProgressAndSuccess)
-            .progress(onProgressAndSuccess)
-            .execute();
+            .progress(onProgressAndSuccess);
 
     }]);
 
---
+++ www/static/js/stream-ajax.ts
@@ -0,0 +1,89 @@
+
+module Ajax {
+
+    export interface IStreamAjaxCallback {
+        (...arguments : any[]) : void;
+    }
+
+    export class StreamAjax {
+
+        private _url : string;
+        private _method : string;
+        private _onError : IStreamAjaxCallback;
+        private _onProgress : IStreamAjaxCallback;
+        private _onSuccess : IStreamAjaxCallback;
+
+        private static NopFunc() : void {}
+
+        constructor(url : string, method? : string) {
+
+            this._method = method || 'post';
+            this._url = url;
+
+            this._onError = StreamAjax.NopFunc;
+            this._onProgress = StreamAjax.NopFunc;
+            this._onSuccess = StreamAjax.NopFunc;
+
+        }
+
+        public error(callback : IStreamAjaxCallback) : StreamAjax {
+
+            this._onError = callback;
+
+            return this;
+
+        }
+
+        public progress(callback : IStreamAjaxCallback) : StreamAjax {
+
+            this._onProgress = callback;
+
+            return this;
+
+        }
+
+        public success(callback : IStreamAjaxCallback) : StreamAjax {
+
+            this._onSuccess = callback;
+
+            return this;
+
+        }
+
+        public execute() : StreamAjax {
+
+            var _this = this;
+            var xhr = new XMLHttpRequest();
+            xhr.open(this._method, this._url, true);
+            xhr.onerror = function () {
+
+                _this._onError.apply(this, arguments);
+
+            };
+            xhr.onprogress = function () {
+
+                _this._onProgress.apply(this, arguments);
+
+            };
+            xhr.onreadystatechange = function () {
+
+                if (4 === this.readyState)
+                    _this._onSuccess.apply(this, arguments);
+
+            };
+
+            xhr.send();
+
+            return this;
+        }
+
+        public static post(url : string, data? : any) : StreamAjax {
+
+            var response : StreamAjax = new StreamAjax(url);
+
+            return response.execute();
+
+        }
+
+    }
+}
